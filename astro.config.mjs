9e0059e47b4b0b44d85dcf887e09c0147ae1f4c5
Justin Awrey <awreyjustin@gmail.com>
2022-12-05 18:58:36 -0800

    prettier config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,5 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
