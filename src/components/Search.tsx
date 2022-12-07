ddb291632029c5b2e608d7cc617bf227ee4a0ac9
Justin Awrey <awreyjustin@gmail.com>
2022-12-06 17:45:33 -0800

    asdf

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,17 @@
-import { createSignal } from "solid-js";
+import { createSignal, Show } from "solid-js";
 import search from "../api/search";
 
 const [searchText, setSearchText] = createSignal("");
+const [loading, setLoading] = createSignal(false);
 
 async function gotoFirstRedditResult() {
+  if (!searchText()) return;
+
+  setLoading(true);
   const result = await search(searchText());
 
   if (!result) {
+    setLoading(false);
     return;
   }
 
@@ -14,15 +19,24 @@ async function gotoFirstRedditResult() {
 }
 
 export default function Search() {
+  const Loading = <div>Please hold... &#128222;</div>;
+
   return (
     <div>
-      <input
-        class="border"
-        type="search"
-        value={searchText()}
-        onInput={(e) => setSearchText((e.target as HTMLInputElement).value)}
-      />
-      <button onClick={gotoFirstRedditResult}>Search</button>
+      <Show when={!loading()} fallback={Loading}>
+        <input
+          class="border focus:outline-slate-300"
+          value={searchText()}
+          onInput={(e) => setSearchText((e.target as HTMLInputElement).value)}
+          placeholder="Search all of reddit..."
+        />
+        <button
+          class="border border-orange-400 hover:border-orange-300 px-1 rounded bg-orange-200 hover:bg-orange-100 transition-colors"
+          onClick={gotoFirstRedditResult}
+        >
+          Try my luck
+        </button>
+      </Show>
     </div>
   );
 }
