77889ceb2e8d85d902d832d92be1d21e47f31d74
Justin Awrey <awreyjustin@gmail.com>
2022-12-08 10:32:09 -0800

    fix imports

--- src/components/lib/index.ts
+++ src/components/lib/index.ts
@@ -1,2 +1,2 @@
-export { default as Button } from "./Button.jsx";
-export { default as Input } from "./Input.jsx";
+export { default as Button } from "./Button";
+export { default as Input } from "./Input";
